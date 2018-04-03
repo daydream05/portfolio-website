fa76c1d2629377bd0f3a97ba7570965102d9c181
Vince P <ianparulan@gmail.com>
2018-04-02 21:37:18 -0400

    minor fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,8 +5,3 @@
  */
 
  // You can delete this file if you're not using it
-
-let env = process.env.NODE_ENV || 'development';
-
-// This adds dotenv (for storing environment variables) to gatsby
-require('dotenv').config({path: `./.env.${env}`});
