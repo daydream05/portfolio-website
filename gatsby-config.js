8870aa704fef73a5a80d238fa2c1867089a99d02
Vince P <ianparulan@gmail.com>
2018-04-02 21:16:31 -0400

    added contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,24 @@
+let env = process.env.NODE_ENV || 'development';
+
+// This adds dotenv (for storing environment variables) to gatsby
+require('dotenv').config({path: `./.env.${env}`});
+
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `${process.env.CONTENTFUL_SPACE_ID}`,
+        accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+      },
+    }
+  ],
 };
+
+console.log('hello world');
+console.log(`${process.env.CONTENTFUL_SPACE_ID}`);
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,8 @@
  */
 
  // You can delete this file if you're not using it
+
+let env = process.env.NODE_ENV || 'development';
+
+// This adds dotenv (for storing environment variables) to gatsby
+require('dotenv').config({path: `./.env.${env}`});
