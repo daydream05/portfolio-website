e74f2d96a9d84bb2dc58c978fe519f68dfaf81db
Vince P <ianparulan@gmail.com>
2018-04-04 21:17:31 -0400

    added context on layout
    So album photos template can send the layout the photos inside it.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import normalize from 'normalize.css';
+import Modal from '../components/Modal/Modal';
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Vince Parulan, Porfolio Website"
-      meta={[
-        { name: 'description', content: "This is my portfolio website and it's currently under construction" },
-        { name: 'keywords', content: 'portfolio, projects' },
-      ]}
-    />
-    <Helmet>
-      <link rel="stylesheet" href="https://use.typekit.net/sbz7hal.css" />
-    </Helmet>
-    <div>
-      {children()}
-    </div>
-  </div>
-)
+class TemplateWrapper extends Component {
+  static childContextTypes = {
+    setPosts: PropTypes.func,
+  }
+
+  getChildContext() {
+    // We need this to be able to get posts in our child or grandchildren.
+    // Any child of this parent would have access to setPosts through this.context.setPosts
+    // setPosts allows us to access posts from our child or grandchildren
+    return {
+      setPosts: posts => {
+        this.posts = posts;
+        console.log('i added the post to the parent');
+      },
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet
+          title="Vince Parulan, Porfolio Website"
+          meta={[
+            { name: 'description', content: "This is my portfolio website and it's currently under construction" },
+            { name: 'keywords', content: 'portfolio, projects' },
+          ]}
+        />
+        <Helmet>
+          <link rel="stylesheet" href="https://use.typekit.net/sbz7hal.css" />
+        </Helmet>
+        <div>
+          {this.props.children()}
+        </div>
+        <div>
+        </div>
+      </div>
+    )
+  }
+}
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
--- src/templates/album-photos.js
+++ src/templates/album-photos.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
 import AlbumPhotoRow from '../components/photo/AlbumPhotoRow/AlbumPhotoRow';
@@ -18,64 +18,77 @@ const AlbumContainer = styled.div`
   display: flex;
   justify-content: center;
 `;
-const AlbumTemplate = ({ data }) => {
-  console.log(data);
-  const albumName = data.contentfulPhotoAlbumDuplicate.albumName;
-  const albumCover = data.contentfulPhotoAlbumDuplicate.albumCover.resolutions;
-  const albumDescription = data.contentfulPhotoAlbumDuplicate.albumDescription.internal.content;
-  const albumPhotos = data.contentfulPhotoAlbumDuplicate.albumPhotos || []
+class AlbumTemplate extends Component {
+  static contextTypes = {
+    setPosts: PropTypes.func,
+  }
 
-  const displayPhotos = () => {
-    const photoArray = [];
-    let photoRow = [];
-    let count = 0;
+  constructor(props, context) {
+    super();
+    // We're adding the photos to the parent in `layouts/index.js`
+    context.setPosts(props.data.contentfulPhotoAlbumDuplicate.albumPhotos || []);
+  }
 
-    albumPhotos.map( photo => {
-    // makes sure we're only adding picture that exists
-      if (photo.sizes) {
-        photoArray.push( photo);
-      }
-    });
+  render() {
+    const albumName = this.props.data.contentfulPhotoAlbumDuplicate.albumName;
+    const albumCover = this.props.data.contentfulPhotoAlbumDuplicate.albumCover.resolutions;
+    const albumDescription = this.props.data.contentfulPhotoAlbumDuplicate.albumDescription.internal.content;
+    const albumPhotos = this.props.data.contentfulPhotoAlbumDuplicate.albumPhotos || []
 
-    return (
-      photoArray.map( photo => {
-        if (photoRow.length === 3) {
-          photoRow = [];
-        } 
+    const displayPhotos = () => {
+      const photoArray = [];
+      let photoRow = [];
+      let count = 0;
 
-        photoRow.push(photo);
-        count += 1;
-
-        if (photoRow.length === 3) {
-          return returnRow(photoRow, count);
-        } else if (photoArray.length - count === 0) {
-          return returnRow(photoRow, count);
+      albumPhotos.map( photo => {
+      // makes sure we're only adding picture that exists
+        if (photo.sizes) {
+          photoArray.push( photo);
         }
-      })
-    );
-  }
+      });
 
-  const returnRow = (photos, count) => {
-    console.log(photos, count);
+      return (
+        photoArray.map( photo => {
+          if (photoRow.length === 3) {
+            photoRow = [];
+          } 
+
+          photoRow.push(photo);
+          count += 1;
+
+          if (photoRow.length === 3) {
+            return returnRow(photoRow, count);
+          } else if (photoArray.length - count === 0) {
+            return returnRow(photoRow, count);
+          }
+        })
+      );
+    }
+
+    const returnRow = (photos, count) => {
+      console.log(photos, count);
+      return (
+        <AlbumPhotoRow photos={photos} key={count}/>
+      );
+    }
+
+    let isModal = true;
+
     return (
-      <AlbumPhotoRow photos={photos} key={count}/>
-    );
+      <AlbumTemplateContainer>
+        <div style={{ overflow: 'auto'}}>
+          <h1>{albumName}</h1>
+          <Img resolutions={albumCover} alt={albumName}/>
+          <p>{albumDescription}</p>
+        </div>
+        <AlbumContainer>
+          <AlbumColumn>
+            { displayPhotos()}
+          </AlbumColumn>
+        </AlbumContainer>
+      </AlbumTemplateContainer>
+    )
   }
-
-  return (
-    <AlbumTemplateContainer>
-      <div style={{ overflow: 'auto'}}>
-        <h1>{albumName}</h1>
-        <Img resolutions={albumCover} alt={albumName}/>
-        <p>{albumDescription}</p>
-      </div>
-      <AlbumContainer>
-        <AlbumColumn>
-          { displayPhotos()}
-        </AlbumColumn>
-      </AlbumContainer>
-    </AlbumTemplateContainer>
-  )
 }
 
 export default AlbumTemplate;
