7f38f2fb6cf08275514c01e2f1b3fc7567f6b881
Vince P <ianparulan@gmail.com>
2018-04-15 18:44:23 -0400

    added card for blog

---
+++ src/pages/blog/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+import video from '../../../public/purple-bag.mp4';
+
+const CardContainer = styled.div`
+  color: rgba(0, 0, 0, .75);
+  transition-duration: 0.5s;
+
+  :hover {
+    color: #fff;
+    background-color: black;
+  }
+`;
+const CardImageBlock = styled.div`
+  width: 100%;
+  height: 150px;
+  background-color: #D8D8D8;
+`;
+
+const CardTitle = styled.h3`
+  font-family: Europa;
+  font-weight: 700;
+  font-size: 24px;
+  text-transform: uppercase;
+  margin: 20px 0 10px;
+`;
+
+const CardDescription = styled.p`
+  font-family: Europa-Regular;
+  font-size: 16px;
+`;
+
+const CardContent = styled.div`
+  padding: 0 25px 25px;
+  overflow: auto;
+`;
+
+const Card = () => (
+  <CardContainer>
+    <CardImageBlock />
+    <CardContent>
+      <CardTitle>Insert Title Here</CardTitle>
+      <CardDescription>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod 
+        tincidunt ut laoreet dolore magna 
+        aliquam erat volutpat.
+      </CardDescription>
+    </CardContent>
+    <video src={video} preload="auto" loop autoPlay />
+  </CardContainer>
+);
+
+export default Card;
