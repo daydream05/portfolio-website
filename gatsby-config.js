d330590d4ce77d9c993ea723fe7bb84f8d35c86e
Vince P <ianparulan@gmail.com>
2018-04-20 15:41:21 -0400

    updated packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ let env = process.env.NODE_ENV || 'development';
 // This adds dotenv (for storing environment variables) to gatsby
 require('dotenv').config({path: `./.env.${env}`});
 
-
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
@@ -19,7 +18,21 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-next`,
+    { 
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Vince Parulan",
+        short_name: "Vince Parulan",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#a2466c",
+        display: "minimal-ui",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 };
 
