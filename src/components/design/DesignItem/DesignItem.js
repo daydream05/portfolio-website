eb2d27a113a5201cea33cfd8ca3a7ca85796540c
Vince P <ianparulan@gmail.com>
2018-04-20 15:40:53 -0400

    added design page

---
+++ src/components/design/DesignItem/DesignItem.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const HorizontalCardImage = styled.div`
+  flex: 3;
+  height: 100%;
+  background-color: #D8D8D8;
+  background-image: ${props => `url(${props.image})`};
+  background-position: center center;
+  background-size: cover;
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  ::after {
+    background-color: rgba(0,0,0. 0.15);
+  }
+`;
+
+const HorizontalCard = styled.div`
+  display: flex;
+  flex-direction: ${ props => props.reversed ? 'row-reverse' : 'row' };
+  height: 50vh;
+  width: 100%;
+`;
+
+const HorizontalCardContent = styled.div`
+  flex: 2;
+  background-color: #fff;
+  display: flex;
+  color: #000;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Content = styled.div `
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 400px;
+  z-index: 1;
+`;
+
+const Title = styled.h3`
+  font-size: 64px;
+  margin: 0 20px 10px;
+  font-family: Europa;
+  font-weight: 700;
+`;
+
+const Description = styled.p `
+  font-size: 18px;
+  font-family: Europa;
+  text-align: center;
+`;
+
+const TagDivider = styled.span`
+  margin: 0 15px;
+`;
+
+const Tags = styled.div`
+  font-family: Europa;
+`;
+
+const Card = (props) => (
+  <HorizontalCard reversed={props.reversed} {...props}>
+    <HorizontalCardContent>
+      <Content>
+        <Title>{props.title}</Title>
+        <Tags>
+          <span>mobile</span><TagDivider>|</TagDivider><span>web</span>
+        </Tags>
+        <Description>{props.description}</Description>
+      </Content>
+    </HorizontalCardContent>
+    <HorizontalCardImage />
+  </HorizontalCard>
+);
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  reversed: PropTypes.bool,
+};
+
+Card.defaultProps = {
+  reversed: false,
+}
+
+const DesignItem = (props) => (
+  <div>
+    <Card title="Grabba" description="Mockup for a garage-sharing app" />
+    <Card title = "Grabba" description = "Mockup for a garage-sharing app"  reversed />
+  </div>
+);
+
+export default DesignItem;
--- src/pages/design/index.js
+++ src/pages/design/index.js
@@ -1,33 +1,10 @@
 import React from 'react';
-import Link from 'gatsby-link';
-import Modal from 'react-modal';
+import DesignItem from '../../components/design/DesignItem/DesignItem';
 
-class SecondPage extends React.Component {
-  state = {
-    modalOpen: false,
-  };
+const Design = () => (
+  <div>
+    <DesignItem />
+  </div>
+);
 
-  openModal = () => {
-    this.setState({ modalOpen: true });
-  }
-
-  render() {
-    return (
-        <div>
-          <div style={{ height: '200vh'}}>
-            <h1>Hi from the second page</h1>
-            <p>Welcome to page 2</p>
-            <Link to="/">Go back to the homepage</Link>
-          </div>
-          <div>
-            <p>hey there</p>
-            <button onClick={this.openModal}></button>
-          </div>
-          <Modal isOpen={this.state.modalOpen} posts={this.posts} location={this.props.location}>   
-          </Modal>
-        </div>
-    )
-  }
-} 
-
-export default SecondPage;
+export default Design;
