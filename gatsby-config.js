5f72ab3a152ee57179811cd59474a7c181b750cc
Vince P <ianparulan@gmail.com>
2018-04-20 21:47:34 -0400

    removed manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,18 +20,6 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-next`,
-    { 
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "Vince Parulan",
-        short_name: "Vince Parulan",
-        start_url: "/",
-        background_color: "#f7f0eb",
-        theme_color: "#a2466c",
-        display: "minimal-ui",
-        icon: "src/images/icon.png", // This path is relative to the root of the site.
-      },
-    },
     `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-typography`,
