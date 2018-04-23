21048d08c698d81ed056915016f95ff08c9c2c18
Vince P <ianparulan@gmail.com>
2018-04-22 23:23:51 -0400

    added typist

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,96 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components';
+import Typist from 'react-typist';
 import './index.css';
 import mixedLogo from './vp-logo-mixed.svg';
 import vpLogo from './vp-logo.svg';
 
-const IndexPage = () => (
-  <div className="full-page">
-    <div className="page-container">
-      <div className="left-page">
+const Page = styled.div`
+  height: 100vh;
+  padding-left: 300px;
+  padding-top: 300px;
+  display: flex;
+  flex-direction: column;
+`;
 
-      </div>
-      <div className="right-page">
-        <div className="right-page--container">
-          <p className="right-page-paragraph">I'm currently under construction. You can come back later.</p>
-          <img src={vpLogo} alt="alternate logo" className="alternate-logo" />
-        </div>
-        <div className="alternate-logo__block">
-          
-        </div>
-      </div>
-        <div className="fixed-logo">
-          <img src={mixedLogo} alt="Black and white VP Logo" className="black-logo" />
-          <div className="nav-links">
-            <Link to="/web/" className="nav-link white">web</Link>
-            <span className="vertical-divider white">|</span>
-            <Link to="/video/" className="nav-link white">video</Link>
-            <span className="vertical-divider white">|</span>
-            <Link to="/design/" className="nav-link">ux/ui</Link>
-            <span className="vertical-divider">|</span>
-            <Link to="/photo/" className="nav-link">photo</Link>
-          </div>
-        </div>
-    </div>
-  </div>
-)
+const TypistHeader = styled(Typist)`
+  font-size: 48px;
+  max-width: 800px;
+`;
 
+const TypistSubeader = styled(Typist)`
+  font-size: 24px;
+  margin-top: 16px;
+`;
+
+const Intro = styled.span`
+  /* Hi, I’m Vince. I’m a: */
+  font-size: 48px;
+  font-weight: 700;
+  color: rgba(0,0,0,0.75);
+  margin: 0;
+`;
+
+const SubIntro = styled.span`
+  color: rgba(0,0,0,0.65);
+`;
+
+const SubIntroBlock = styled.div`
+  margin-top: 32px;
+`;
+
+class IndexPage extends Component {
+  state = {
+    renderMsg: false,
+  }
+
+  onHeaderTyped = () => {
+    this.setState({ renderMsg: true });
+  }
+
+  render() {
+    return (
+      <div>
+        <Page>
+            <div>
+              <TypistHeader
+                avgTypingSpeed={40}
+                startDelay={1000}
+                onTypingDone={this.onHeaderTyped}
+                cursor={{ hideWhenDone: true, hideWhenDoneDelay: 0 }}
+              >
+                <Intro>Hi!</Intro>
+                <Typist.Delay ms={500} />
+                <Intro> I'm Vince.</Intro>
+                <Typist.Delay ms={1500} />
+                <br/>
+                <Intro>I'm a creative developer,</Intro>
+                <Typist.Delay ms={1500} />
+                <br/>
+                <Intro>And I build websites</Intro>
+                <Typist.Backspace count={14} delay={1000} avgTypingSpeed={60}/>
+                <Intro>make videos</Intro>
+                <Typist.Backspace count={11} delay={1000} avgTypingSpeed={60}/>
+                <Intro>design interfaces.</Intro>
+                <Typist.Backspace count={20} delay={1000} avgTypingSpeed={60}/>
+                <Intro>a little bit of everything.</Intro>
+                
+              </TypistHeader>
+            </div>
+            {this.state.renderMsg ? (
+              <SubIntroBlock>
+                <TypistSubeader
+                  startDelay={1000}
+                >
+                  <SubIntro>Feel free to check out my work before I start adding a paywall </SubIntro>
+                  <span>🙃</span>
+                </TypistSubeader>
+              </SubIntroBlock>
+            ) : null }
+        </Page>
+      </div>
+    );
+  }
+}
 export default IndexPage;
