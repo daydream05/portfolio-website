d697344ff8f79c08e912695837551ca892dc80e1
Vince P <ianparulan@gmail.com>
2018-11-22 15:48:41 -0500

    remove update scroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,16 +5,3 @@
  */
 
  // You can delete this file if you're not using it
-
-const windowWidth = window.innerWidth
-
-exports.shouldUpdateScroll = args => {
-  // Scroll position only matters on mobile as on larger screens, we use a modal.
-  if (windowWidth < 750) {
-    return true;
-  }
-  else {
-    console.log('it didnt update');
-    return false;
-  }
-}
